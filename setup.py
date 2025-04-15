edc99056a55474b718e8fbf80d739d12490a3e01
Sebastian Tabares <sytabaresa@gmail.com>
2025-04-15 16:52:23 +0800

    add setup.cfg

--- setup.py
+++ setup.py
@@ -1,30 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setup(
-    name="webpage",
-    version="0.1.2",
-    author="Sebastian Tabares Amaya",
-    author_email="me@syta.co",
-    description="A package for processing HTML files and web content",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/EPI3co/webpage.git",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
-    install_requires=[
-        # Add your dependencies here
-    ],
-    entry_points={
-        "console_scripts": [
-            "webpage=webpage.main:main",
-        ],
-    },
-)
+setup()
